45a9eb741ac5de3f340b2cb85be7c3d0e61bcc49
vayntail <113714157+vayntail@users.noreply.github.com>
2024-11-26 15:55:07 -0500

    setup basic home view using jsx-view-engine

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const jsxViewEngine = require("jsx-view-engine");
+
+// set static files to public folder
+app.use(express.static("public"));
+
+// set up view engine
+app.set("view engine", "jsx");
+app.set("views", "./views");
+app.engine("jsx", jsxViewEngine());
+
+// render home page
+app.get("/", (req, res) => {
+  res.render("Home");
+});
 
 // start server
 app.listen(port, () => {
---
+++ views/Home.jsx
@@ -0,0 +1,16 @@
+const React = require("react");
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <head>
+          <link rel="stylesheet" href="/style.css" />
+        </head>
+        <h1>This is the home page.</h1>
+      </>
+    );
+  }
+}
+
+module.exports = Home;
